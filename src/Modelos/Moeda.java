3f7c9870541375314ffd6449a6b4efc1ed7df0a9
JefersonBLuz <jefersonbrluz@gmail.com>
2026-03-10 15:00:05 -0300

    feat: Expandindo a classe Moeda com novos atributos e métodos para conversão de moedas

--- src/Modelos/Moeda.java
+++ src/Modelos/Moeda.java
@@ -1,9 +1,40 @@
 package Modelos;
 
-import java.lang.reflect.Array;
-
 public class Moeda {
-    private String baseCode;
     private String result;
-    private Array[] conversorRates;
+    private String base_code;
+    private String target_code;
+    private double conversion_rate;
+    private double conversion_result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public String getTarget_code() {
+        return target_code;
+    }
+
+    public double getConversion_rate() {
+        return conversion_rate;
+    }
+
+    public double getConversion_result() {
+        return conversion_result;
+    }
+
+    @Override
+    public String toString() {
+        return "Moeda{" +
+                "result='" + result + '\'' +
+                ", base_code='" + base_code + '\'' +
+                ", target_code='" + target_code + '\'' +
+                ", conversion_rate=" + conversion_rate +
+                ", conversion_result=" + conversion_result +
+                '}';
+    }
 }
